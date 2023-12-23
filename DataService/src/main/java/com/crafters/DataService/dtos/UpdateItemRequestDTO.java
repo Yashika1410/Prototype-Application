2e51fa880840a6edddf232787f4bb084001cc5ec
Prapti <prapti@nagarro.com>
2023-12-23 17:00:35 +0530

    Backend code for update Item

--- DataService/src/main/java/com/crafters/DataService/dtos/UpdateItemRequestDTO.java
+++ DataService/src/main/java/com/crafters/DataService/dtos/UpdateItemRequestDTO.java
@@ -1,2 +1,18 @@
-package com.crafters.DataService.dtos;public class UpdateItemRequestDTO {
+package com.crafters.DataService.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateItemRequestDTO {
+    private String id;
+    private String name;
+    private String rowType;
+    private String collectionName;
+    private Map<String,Object> attributes;
+    private Map<String, Integer> yearValue;
 }
